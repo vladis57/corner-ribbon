25f48a2825cc3f5dbea801f874ca6cf6f80db68b
Vlad Flonta <vlad.flonta@gmail.com>
2015-10-20 01:27:14 +0300

    Added citation and app logo

--- ribbon.js
+++ ribbon.js
@@ -12,10 +12,10 @@
 
 	ready(function () {
 		var sqrt2 = Math.sqrt(2), ribbon;
-		if(INSTALL_OPTIONS['link']) {
+		if (INSTALL_OPTIONS['link']) {
 			ribbon = document.createElement("a");
 			ribbon.setAttribute('href', INSTALL_OPTIONS['link']);
-			if(INSTALL_OPTIONS['new_tab']) {
+			if (INSTALL_OPTIONS['new_tab']) {
 				ribbon.setAttribute('target', '_blank');
 			}
 		} else {
@@ -38,8 +38,8 @@
 		var textSize = measureText(INSTALL_OPTIONS['text'], s.getPropertyValue("font-size"), '');
 		var rowCount = Math.ceil(textSize.width / INSTALL_OPTIONS['width']),
 			rowWidth = 0, row = [], rows = [], word = 0;
-		while(rows.length < rowCount && word < words.length) {
-			if( rowWidth + words[word].width  >= INSTALL_OPTIONS['width'] - (positionParts[0] === 'top' ? rowCount - rows.length : rows.length) * 2 * textSize.height
+		while (rows.length < rowCount && word < words.length) {
+			if (rowWidth + words[word].width >= INSTALL_OPTIONS['width'] - (positionParts[0] === 'top' ? rowCount - rows.length : rows.length) * 2 * textSize.height
 				&& (rows.length + 1) * textSize.height + 20 < INSTALL_OPTIONS['width']) {
 				rows.push(row.join(' '));
 				rowWidth = 0;
@@ -49,28 +49,36 @@
 			row.push(words[word].text);
 			word++;
 		}
-		if(row.length) {
-			if(rows.length < rowCount && (rows.length + 1) * textSize.height + 20 < INSTALL_OPTIONS['width']) {
+		if (row.length) {
+			if (rows.length < rowCount && (rows.length + 1) * textSize.height + 20 < INSTALL_OPTIONS['width']) {
 				rows.push(row.join(' '));
 			} else {
 				rows[rows.length - 1] += ' ...';
 			}
 		}
 		ribbon.innerHTML = rows.join('<br />');
-		
+
 		var deviation = ((ribbon.clientWidth / 2 + ribbon.clientHeight / 2) / sqrt2) - ribbon.clientHeight * sqrt2;
-		if(positionParts[0] === 'top') {
+		if (positionParts[0] === 'top') {
 			ribbon.style.top = deviation + 'px';
 		} else {
 			ribbon.style.bottom = deviation + 'px';
 		}
-		if(positionParts[1] === 'right') {
+		if (positionParts[1] === 'right') {
 			ribbon.style.right = (((INSTALL_OPTIONS['width'] - 1000) / 1000) * deviation - ribbon.clientHeight * sqrt2 / 2).toFixed(2) + 'px';
 		} else {
 			ribbon.style.left = (((INSTALL_OPTIONS['width'] - 1000) / 1000) * deviation - ribbon.clientHeight * sqrt2 / 2).toFixed(2) + 'px';
 		}
 	});
 
+	/**
+	 * Uses canvas.measureText to compute and return the width of the given text of given font in pixels.
+	 * 
+	 * @param {String} text The text to be rendered.
+	 * @param {String} font The css font descriptor that text is to be rendered with (e.g. "bold 14px verdana").
+	 * 
+	 * @see http://stackoverflow.com/questions/118241/calculate-text-width-with-javascript/21015393#21015393
+	 */
 	function getTextWidth(text, font) {
 		var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
 		var context = canvas.getContext("2d");
@@ -79,6 +87,9 @@
 		return metrics.width;
 	}
 
+	/*
+	 * @see http://stackoverflow.com/a/4032497
+	 */
 	function measureText(pText, pFontSize, pStyle) {
 		var lDiv = document.createElement('lDiv');
 
@@ -105,6 +116,27 @@
 		return lResult;
 	}
 
+	/**
+	 * (c) Michael Zelensky 2015
+	 *
+	 * DESCRIPTION
+	 *
+	 * Returns maximum z-index on page for all elements
+	 *
+	 * LICENSE
+	 *
+	 * Distributed under MIT License
+	 * You can use this code in your project without limitation, no matter if it is commercial or not,
+	 * with all copyright marks intact
+	 *
+	 * USAGE
+	 *
+	 * Just include this code into your JS or HTML and then
+	 *
+	 * var maxZIndex = getMaxZIndex();
+	 * 
+	 * @see https://github.com/MichaelZelensky/jsLibraries/blob/master/getMaxZIndex.js
+	 */
 	var getMaxZIndex = function () {
 		var zIndex, maxZ = 0,
 			all = document.getElementsByTagName('*');
